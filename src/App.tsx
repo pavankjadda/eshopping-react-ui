1b4ebde1034ce002e71e20439166c1127600667a
Pavan Jadda <pavankjadda@users.noreply.github.com>
2020-12-24 23:08:04 -0500

    Adds login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import './App.css';
 import Header from "./layout/header";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Login from "./core/login";
 
 class App extends React.Component
 {
     render()
     {
         return (
-            <div className="App">
-                <Header/>
-            </div>
+            <Router>
+                <div className="App">
+                    <Header/>
+                </div>
+
+                <Switch>
+                    <Route exact path="/login">
+                        <Login />
+                    </Route>
+                </Switch>
+            </Router>
         );
     }
 }
--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -17,6 +18,9 @@ const useStyles = makeStyles((theme: Theme) =>
         },
         title: {
             flexGrow: 1,
+        },
+        customLink: {
+            color: 'white',
         },
     }),
 );
@@ -34,7 +38,11 @@ export default function Header()
                     <Typography variant="h6" className={classes.title}>
                         eShopping
                     </Typography>
-                    <Button color="inherit">Login</Button>
+                    <Button color="inherit">
+                        <Link className={classes.customLink} to="/login">
+                            Login
+                        </Link>
+                    </Button>
                 </Toolbar>
             </AppBar>
         </div>
